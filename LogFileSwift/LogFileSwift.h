1fba1b2ce05ca076a496ccbcb732228e0a6232c6
minhbh <minhbh.paraine@gmail.com>
2023-08-24 09:29:18 +0700

    Initial Commit

---
+++ LogFileSwift/LogFileSwift.h
@@ -0,0 +1,18 @@
+//
+//  LogFileSwift.h
+//  LogFileSwift
+//
+//  Created by Paraline App on 24/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LogFileSwift.
+FOUNDATION_EXPORT double LogFileSwiftVersionNumber;
+
+//! Project version string for LogFileSwift.
+FOUNDATION_EXPORT const unsigned char LogFileSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogFileSwift/PublicHeader.h>
+
+
